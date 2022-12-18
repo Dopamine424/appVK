54f295acb0d48d9bd0eee0768dd137d4088a7ff8
Alexandr <fomin02@inbox.ru>
2022-12-18 12:09:01 +0500

    com-4

--- public/script.js
+++ public/script.js
@@ -1,6 +1,24 @@
 var templateItem = document.getElementById("item");
 var svg = document.getElementById("arrow");
-
+x = 0;
 templateItem.onclick = function() {
-    svg.classList.add('rotate');
+    x+=180;
+    svg.style.transform = 'rotate(' + x + 'deg)';
+}
+
+var templateItem2 = document.getElementById("item2");
+var svg2 = document.getElementById("arrow2");
+let x2 = 0;
+templateItem2.onclick = function() {
+    x2+=180;
+    svg2.style.transform = 'rotate(' + x2 + 'deg)';
+}
+
+
+var templateItem3 = document.getElementById("item3");
+var svg3 = document.getElementById("arrow3");
+let x3 = 0;
+templateItem3.onclick = function() {
+    x3+=180;
+    svg3.style.transform = 'rotate(' + x3 + 'deg)';
 }
